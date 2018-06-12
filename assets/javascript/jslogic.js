e717b555699c1015894cdf8dc7ca6072f10d746f
ChristoNik <39217571+ChristoNik@users.noreply.github.com>
2018-06-11 23:21:50 -0400

    Add files via upload

---
+++ assets/javascript/jslogic.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    //dynamically display current time
+   function getDateTimeFunction() {
+     var dateTime = new Date();
+       $("#realTime").html(dateTime);
+    }
+    $(document).ready(function(){
+       setInterval(getDateTimeFunction, 1000);
+    });
+//end dynamicly display
+
+
+    //1. Firebase code goes here
+    var config = {
+        apiKey: "AIzaSyAoxAE3hiZvDBwrcSYiFiL5qjuDHMNc3VA",
+        authDomain: "train-80f7a.firebaseapp.com",
+        databaseURL: "https://train-80f7a.firebaseio.com",
+        projectId: "train-80f7a",
+        storageBucket: "",
+        messagingSenderId: "185770742746"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    //2. Button for adding trains
+    $("#submit").on("click", function() {
+
+        //capture user input
+        var train = $("#trainName").val().trim();
+        var dest = $("#destination").val().trim();
+        var first = $("#firstTrain").val().trim();
+        var freq = $("#frequency").val().trim();
+
+        //upload to the database
+        database.ref().push({
+            train: train,
+            dest: dest,
+            first: first,
+            freq: freq
+        });
+
+        //clear the input boxes
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#firstTrain").val("");
+        $("#frequency").val("");
+});
+
+    //3. Firebase event to capture train added to database and a row in html when user adds train
+database.ref().on("child_added", function (childSnapshot) {
+    var train = childSnapshot.val().train;
+    var dest = childSnapshot.val().dest;
+    var first = childSnapshot.val().first;
+    var freq = childSnapshot.val().freq;
+
+        var freq = parseInt(freq);
+        //current Time
+        var currentTime = moment();
+        var dConverted = moment(childSnapshot.val().time, 'HH:mm').subtract(1, 'years');
+        var trainTime = moment(dConverted).format('HH:mm');
+
+        //difference
+        var tConverted = moment(trainTime, 'HH:mm').subtract(1, 'years');
+        var tDifference = moment().diff(moment(tConverted), 'minutes');
+        var tRemainder = tDifference % freq;
+        var minsAway = freq - tRemainder;
+        var nextTrain = moment().add(minsAway, 'minutes');
+
+
+        //Append table
+        $('#currentTime').text(currentTime);
+        $('#trainTable').append(
+            "<tr><td id='trainDisplay'>" + childSnapshot.val().train +
+            "</td><td id='destDisplay'>" + childSnapshot.val().dest +
+            "</td><td id='freqDisplay'>" + childSnapshot.val().freq +
+            "</td><td id='firstDisplay'>" + moment(nextTrain).format("HH:mm") +
+            "</td><td id='awayDisplay'>" + minsAway  + ' minutes until arrival' + "</td></tr>");
+    });
+
+});
